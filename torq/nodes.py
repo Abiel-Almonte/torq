a47b4fae7ee76415380ffb498c9d7c4dc2c49f8d
Abiel Almonte <almonteabiel@gmail.com>
2026-01-10 02:00:42 -0500

    Move dag node into node file

---
+++ torq/nodes.py
@@ -0,0 +1,42 @@
+from typing import Tuple
+
+from .pipes import Pipe
+
+
+class DAGNode:
+    def __init__(
+        self, node_id: str, stream_id: int, pipe: Pipe, args: Tuple["DAGNode", ...]
+    ):
+        self.node_id = node_id
+        self.stream_id = stream_id
+        self.pipe = pipe
+        self.args = args
+
+    def __call__(self, *args):
+        return self.pipe(*args)
+
+    def __hash__(self) -> int:
+        return hash(self.node_id)
+
+    def __repr__(self) -> str:
+        def _repr(node: DAGNode, level=0):
+            space = "\t"
+            indent = space * level
+
+            s = f"{indent}DAGNode(\n"
+            s += f"{indent}{space}node_id={node.node_id},\n"
+            s += f"{indent}{space}stream={node.stream_id},\n"
+            s += f"{indent}{space}pipe={node.pipe.__class__.__name__},\n"
+
+            if node.args:
+                s += f"{indent}{space}args=(\n"
+                for arg in node.args:
+                    s += _repr(arg, level + 2) + ",\n"
+                s += f"{indent}{space})\n"
+            else:
+                s += f"{indent}{space}args=()\n"
+
+            s += f"{indent})"
+            return s
+
+        return _repr(self)
