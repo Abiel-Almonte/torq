501a4be37e6375aad57cb536082127f76dc140ab
Abiel Almonte <almonteabiel@gmail.com>
2026-01-11 02:04:01 -0500

    add HAS_CUDA for future tasks

--- torq/__init__.py
+++ torq/__init__.py
@@ -2,3 +2,11 @@ from .registry import register, register_decorator
 from .pipeline import Sequential, Concurrent
 from .pipes import Pipe
 from .compiler import register_backend, compile
+from .utils import logging
+
+try:
+    import _torq
+    HAS_CUDA = True
+except ImportError:
+    HAS_CUDA = False
+    logging.warning("CUDA extension is not available. Falling back to CPU-only features.")
