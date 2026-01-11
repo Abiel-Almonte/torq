24e1cdc0614cd0734b6c800d72d78699353fad13
Abiel Almonte <almonteabiel@gmail.com>
2026-01-11 14:01:35 -0500

    silent import error

--- torq/__init__.py
+++ torq/__init__.py
@@ -2,11 +2,11 @@ from .registry import register, register_decorator
 from .pipeline import Sequential, Concurrent
 from .pipes import Pipe
 from .compiler import register_backend, compile
-from .utils import logging
+
+HAS_CUDA = False
 
 try:
-    import _torq
+    from . import cuda
     HAS_CUDA = True
 except ImportError:
-    HAS_CUDA = False
-    logging.warning("CUDA extension is not available. Falling back to CPU-only features.")
+    pass
