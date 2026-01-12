40d516942946d4d9984ea1bad39518dfaef6b580
Abiel Almonte <almonteabiel@gmail.com>
2026-01-12 01:42:43 -0500

    refactor dag into dag the struct and the lowering function

---
+++ torq/compiler/dag.py
@@ -0,0 +1,70 @@
+from typing import Tuple
+
+from ..runnable import Runnable
+from ..pipes import Input, Output
+from ..pipeline import System
+
+from .nodes import DAGNode
+from .frontend import lower
+
+class DAG(Runnable):
+    def __init__(self, nodes: Tuple[DAGNode, ...], leaves: Tuple[DAGNode, ...]) -> None:
+        self.nodes = nodes
+        self.leaves = leaves
+
+    @staticmethod
+    def from_system(system: System) -> "DAG":
+        dag = DAG(*lower(system))
+        dag.semantic_lint()  # API make cycles and orphans impossible to occur.
+        return dag
+
+    def semantic_lint(self):
+        for node in self:
+            if isinstance(node.pipe, Input):
+                if len(node.args) > 0:
+                    raise RuntimeError(
+                        f"Input node {node.node_id} cannot have incoming edges"
+                    )
+
+            for arg in node.args:
+                if isinstance(arg.pipe, Output):
+                    raise RuntimeError(
+                        f"Output node {arg.node_id} cannot have outgoing edges"
+                    )
+
+    def __call__(self, *args):
+        cache = {}
+
+        for node in self:
+            ins = tuple(cache[arg] for arg in node.args)
+            cache[node] = node(*ins)
+
+        outs = tuple(cache[leaf] for leaf in self.leaves)
+
+        return outs[0] if len(outs) == 1 else outs
+
+    def __iter__(self):
+        visited = set()
+
+        def visit(node: DAGNode):
+            if node in visited:
+                return
+
+            # visit up the tree through the args
+            for arg in node.args:
+                yield from visit(arg)
+
+            yield node
+            visited.add(node)
+
+        for leaf in self.leaves:
+            yield from visit(leaf)
+
+    def __repr__(self) -> str:
+        line = "=" * 80
+        return f"\n".join(
+            [
+                f"Tree {i}:\n{line}\n{repr(leaf)}\n{line}"
+                for i, leaf in enumerate(self.leaves)
+            ]
+        )
--- torq/compiler/frontend.py
+++ torq/compiler/frontend.py
@@ -1,76 +1,13 @@
 from typing import Optional, Union, Tuple
 from collections import defaultdict
 
-from .runnable import Runnable
-from .pipes import Pipe, Input, Output
-from .pipeline import System, Pipeline, Sequential, Concurrent
+from ..pipes import Pipe
+from ..pipeline import System, Pipeline, Sequential, Concurrent
+
+from .. import config
+from ..utils import logging
+
 from .nodes import DAGNode
-
-from .utils import logging
-from . import config
-
-
-class DAG(Runnable):
-    def __init__(self, nodes: Tuple[DAGNode, ...], leaves: Tuple[DAGNode, ...]) -> None:
-        self.nodes = nodes
-        self.leaves = leaves
-
-    @staticmethod
-    def from_system(system: System) -> "DAG":
-        dag = DAG(*_build_dag_from_system(system))
-        dag.semantic_lint()  # API make cycles and orphans impossible to occur.
-        return dag
-
-    def semantic_lint(self):
-        for node in self:
-            if isinstance(node.pipe, Input):
-                if len(node.args) > 0:
-                    raise RuntimeError(
-                        f"Input node {node.node_id} cannot have incoming edges"
-                    )
-
-            for arg in node.args:
-                if isinstance(arg.pipe, Output):
-                    raise RuntimeError(
-                        f"Output node {arg.node_id} cannot have outgoing edges"
-                    )
-
-    def __call__(self, *args):
-        cache = {}
-
-        for node in self:
-            ins = tuple(cache[arg] for arg in node.args)
-            cache[node] = node(*ins)
-
-        outs = tuple(cache[leaf] for leaf in self.leaves)
-
-        return outs[0] if len(outs) == 1 else outs
-
-    def __iter__(self):
-        visited = set()
-
-        def visit(node: DAGNode):
-            if node in visited:
-                return
-
-            # visit up the tree through the args
-            for arg in node.args:
-                yield from visit(arg)
-
-            yield node
-            visited.add(node)
-
-        for leaf in self.leaves:
-            yield from visit(leaf)
-
-    def __repr__(self) -> str:
-        line = "=" * 80
-        return f"\n".join(
-            [
-                f"Tree {i}:\n{line}\n{repr(leaf)}\n{line}"
-                for i, leaf in enumerate(self.leaves)
-            ]
-        )
 
 
 class StreamAssigner:
@@ -83,7 +20,7 @@ class StreamAssigner:
         return self._idx
 
 
-def _build_dag_from_system(system: System):
+def lower(system: System):
     nodes = tuple()
     assigner = StreamAssigner(config.max_streams)
     global_cnt = defaultdict(int)
